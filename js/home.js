d3c880c49e4acf308dc81ee34e3834522aaa867f
huyanh-code <huyanh3012711@gmail.com>
2026-05-19 10:05:43 +0700

    thay thế hard code sử dụng mock js để view UI

---
+++ js/home.js
@@ -0,0 +1,62 @@
+/**
+ * Định dạng giá tiền theo định dạng Việt Nam
+ * @param {*} price 
+ * @returns 
+ */
+function formatPrice(price) {
+  return price.toLocaleString("vi-VN");
+}
+
+/**
+ * Hàm render sản phẩm theo category vào container tương ứng
+ * @param {*} category 
+ * @param {*} containerId 
+ */
+function renderProducts(category, containerId) {
+  const container = document.getElementById(containerId);
+
+  // lọc sản phẩm theo category
+  const filtered = products.filter((p) => p.category === category);
+
+  // render sản phẩm ra UI
+  // mỗi sản phẩm sẽ là một thẻ a, bên trong có hình ảnh, tên và giá tiền
+  // nếu có oldPrice thì hiển thị giá cũ, nếu không thì chỉ hiển thị giá hiện tại
+  // khi click vào sản phẩm sẽ chuyển đến trang product.html với query param là id của sản phẩm đó
+  // forEach sẽ duyệt qua từng sản phẩm đã lọc được và thêm vào container dưới dạng HTML
+  // innerHTML += sẽ thêm nội dung HTML mới vào cuối nội dung hiện tại của container, tạo thành một danh sách sản phẩm liên tiếp
+  filtered.forEach((p) => {
+    container.innerHTML += `
+        <a href="product.html?id=${p.id}" class="spham">
+        <img src="${p.image}" alt="${p.name}">
+        
+
+        <div>
+          <h3>${p.name}</h3>
+        </div>
+
+        <div class="tien">
+          <div class="gia">
+            <p class="text-gia">
+              ${formatPrice(p.price)}<u>đ</u>
+
+              ${
+                p.oldPrice
+                  ? `
+                <del>${formatPrice(p.oldPrice)}đ</del>
+              `
+                  : ""
+              }
+            </p>
+          </div>
+
+          <button class="hang">
+            <img src="../assets/image/shopping-cart.svg">
+          </button>
+        </div>
+        </a>
+    `;
+  });
+}
+
+renderProducts("mousse", "mousse");
+renderProducts("bread salty", "bread salty");
---
+++ js/product-detail.js
@@ -0,0 +1,23 @@
+function formatPrice(price) {
+  return price.toLocaleString("vi-VN");
+}
+
+// lấy id từ URL
+const params = new URLSearchParams(window.location.search);
+const id = Number(params.get("id"));
+
+// tìm sản phẩm
+const product = products.find(p => p.id === id);
+
+// render ra UI
+if (product) {
+  document.getElementById("product-image").src = product.image;
+  document.getElementById("product-name").innerText = product.name;
+  document.getElementById("product-price").innerText =
+    formatPrice(product.price) + "đ";
+
+  document.getElementById("product-desc").innerText =
+    product.desc || "Không có mô tả";
+} else {
+  document.body.innerHTML = "<h2>Không tìm thấy sản phẩm</h2>";
+}
